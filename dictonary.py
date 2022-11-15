677b6edb294114174b572742fc6d62f4c1b794ea
AARTHY K P <105295898+aarthykp6@users.noreply.github.com>
2022-11-15 21:50:40 +0530

    Different Set Operations

---
+++ dictonary.py
@@ -0,0 +1,16 @@
+# Program to perform different set operations
+
+A = {0, 1, 2, 3, 4};
+B = {2, 4, 5, 6, 8};
+
+# union
+print("Union :", A | B)
+
+# intersection
+print("Intersection :", A & B)
+
+# difference
+print("Difference :", A - B)
+
+# symmetric difference
+print("Symmetric difference :", A ^ B)
